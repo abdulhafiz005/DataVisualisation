6e7b8c317287b443dbe3804e806ead577774763c
abdul_hafiz <creativedraftsman@gmail.com>
2021-01-22 12:56:31 +0500

    functions and modules

---
+++ textutilites.py
@@ -0,0 +1,7 @@
+def charCount(paragraph):
+    """This funtion accepts a text string and returns an integer count of numbers of characters in it."""
+    return len(paragraph)
+
+def wordCount(paragraph):
+    """This funtion accepts a text string and returns an integer count of numbers of words in it."""
+    return len(paragraph.split(" "))
